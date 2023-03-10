a179daa38af864d5c7b1a04baa0cefa07761b4d3
Art D <adpm@list.ru>
2023-03-11 02:00:50 +0600

    Create Snackbar while user not exist

--- app/src/main/java/com/example/onlineshop/presentation/screens/Login.kt
+++ app/src/main/java/com/example/onlineshop/presentation/screens/Login.kt
@@ -24,6 +24,7 @@ import com.example.onlineshop.presentation.navigation.AppDestination
 import com.example.onlineshop.presentation.navigation.Page1Screen
 import com.example.onlineshop.presentation.navigation.navigateSingleTopTo
 import com.example.onlineshop.presentation.viewmodel.MainViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun Login(
@@ -33,100 +34,111 @@ fun Login(
 ) {
 
     val state by vm.viewState.collectAsState()
+    val scaffoldState = rememberScaffoldState()
 
     LaunchedEffect(state.isLogged) {
         if (state.isLogged) {
             navController.navigateSingleTopTo(Page1Screen.route)
         }
     }
-
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        var firstName by remember { mutableStateOf("") }
-        var password by remember { mutableStateOf("") }
-        var showPassword by remember { mutableStateOf(false) }
-
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
+    Scaffold(
+        scaffoldState = scaffoldState
+    ) { paddingValues ->
+        Surface(
+            color = MaterialTheme.colors.primary,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
         ) {
+            val scope = rememberCoroutineScope()
+            var firstName by remember { mutableStateOf("") }
+            var password by remember { mutableStateOf("") }
+            var showPassword by remember { mutableStateOf(false) }
 
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
             ) {
-                Box(
-                    modifier = Modifier
-                        .padding(bottom = 50.dp)
-                        .width(200.dp)
-                        .height(50.dp)
-                ) {
-                    Text(
-                        text = "Welcome back",
-                        fontFamily = FontFamily(Font(R.font.montserrat, FontWeight.Bold)),
-                        fontWeight = FontWeight.W600,
-                        fontSize = 26.sp,
-                        letterSpacing = (-0.3).sp,
-                        lineHeight = 31.69.sp,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        color = Color.Black
-                    )
-                }
 
                 Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier
-                        .padding(horizontal = 44.dp)
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    val textStyle = TextStyle(
-                        fontFamily = FontFamily(Font(R.font.montserrat, FontWeight.Normal)),
-                        fontWeight = FontWeight.W500,
-                        fontSize = 11.sp,
-                        lineHeight = 13.sp,
-                        textAlign = TextAlign.Start,
-                        letterSpacing = (-0.3).sp,
-                        color = Color(0xFF7B7B7B)
-                    )
-                    TextField(
-                        value = firstName,
-                        onValueChange = { firstName = it },
+                    Box(
                         modifier = Modifier
+                            .padding(bottom = 50.dp)
+                            .width(200.dp)
                             .height(50.dp)
-                            .fillMaxWidth(),
-                        shape = RoundedCornerShape(15.dp),
-                        placeholder = {
-                            Text(
-                                modifier = Modifier
-                                    .fillMaxSize(),
-                                text = "First name",
-                                fontFamily = FontFamily(Font(R.font.montserrat, FontWeight.Normal)),
-                                fontWeight = FontWeight.W500,
-                                fontSize = 11.sp,
-                                lineHeight = 13.sp,
-                                textAlign = TextAlign.Center,
-                                letterSpacing = (-0.3).sp,
-                                color = Color(0xFF7B7B7B)
-                            )
-                        },
-                        colors = TextFieldDefaults.textFieldColors(
-                            unfocusedIndicatorColor = Color.Transparent,
-                            backgroundColor = Color(0xFFE8E8E8)
-                        ),
-                        textStyle = textStyle
-                    )
-                    Spacer(modifier = Modifier.height(30.dp))
-                    TextField(
-                        value = password,
-                        onValueChange = { password = it },
+                    ) {
+                        Text(
+                            text = "Welcome back",
+                            fontFamily = FontFamily(Font(R.font.montserrat, FontWeight.Bold)),
+                            fontWeight = FontWeight.W600,
+                            fontSize = 26.sp,
+                            letterSpacing = (-0.3).sp,
+                            lineHeight = 31.69.sp,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            color = Color.Black
+                        )
+                    }
+
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
                         modifier = Modifier
-                            .height(50.dp)
-                            .fillMaxWidth(),
-                        shape = RoundedCornerShape(15.dp),
-                        placeholder = {
+                            .padding(horizontal = 44.dp)
+                    ) {
+                        val textStyle = TextStyle(
+                            fontFamily = FontFamily(Font(R.font.montserrat, FontWeight.Normal)),
+                            fontWeight = FontWeight.W500,
+                            fontSize = 11.sp,
+                            lineHeight = 13.sp,
+                            textAlign = TextAlign.Start,
+                            letterSpacing = (-0.3).sp,
+                            color = Color(0xFF7B7B7B)
+                        )
+                        TextField(
+                            value = firstName,
+                            onValueChange = { firstName = it },
+                            modifier = Modifier
+                                .height(50.dp)
+                                .fillMaxWidth(),
+                            shape = RoundedCornerShape(15.dp),
+                            placeholder = {
+                                Text(
+                                    modifier = Modifier
+                                        .fillMaxSize(),
+                                    text = "First name",
+                                    fontFamily = FontFamily(
+                                        Font(
+                                            R.font.montserrat,
+                                            FontWeight.Normal
+                                        )
+                                    ),
+                                    fontWeight = FontWeight.W500,
+                                    fontSize = 11.sp,
+                                    lineHeight = 13.sp,
+                                    textAlign = TextAlign.Center,
+                                    letterSpacing = (-0.3).sp,
+                                    color = Color(0xFF7B7B7B)
+                                )
+                            },
+                            colors = TextFieldDefaults.textFieldColors(
+                                unfocusedIndicatorColor = Color.Transparent,
+                                backgroundColor = Color(0xFFE8E8E8)
+                            ),
+                            textStyle = textStyle
+                        )
+                        Spacer(modifier = Modifier.height(30.dp))
+                        TextField(
+                            value = password,
+                            onValueChange = { password = it },
+                            modifier = Modifier
+                                .height(50.dp)
+                                .fillMaxWidth(),
+                            shape = RoundedCornerShape(15.dp),
+                            placeholder = {
 
                                 Text(
                                     modifier = Modifier
@@ -148,68 +160,86 @@ fun Login(
                                 )
 
 
-                        },
-                        colors = TextFieldDefaults.textFieldColors(
-                            unfocusedIndicatorColor = Color.Transparent,
-                            backgroundColor = Color(0xFFE8E8E8)
-                        ),
-                        textStyle = textStyle,
-                        visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
-                        trailingIcon = {
-                            IconButton(
-                                onClick = { showPassword = !showPassword },
-                                content = {
-                                    Icon(
-                                        painter = painterResource(id = if (showPassword) R.drawable.eye_visible else R.drawable.eye_hidden),
-                                        contentDescription = if (showPassword) "Hide password" else "Show password"
-                                    )
-                                }
-                            )
-                        }
-                    )
-                }
-
-                Box(
-                    modifier = Modifier
-                        .padding(top = 50.dp, start = 44.dp, end = 44.dp)
-                        .fillMaxWidth()
-                ) {
-                    Column(
-                    ) {
-                        Button(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(50.dp),
-                            colors = ButtonDefaults.buttonColors(
-                                backgroundColor = Color(0xFF4E55D7),
-                                contentColor = Color.White
+                            },
+                            colors = TextFieldDefaults.textFieldColors(
+                                unfocusedIndicatorColor = Color.Transparent,
+                                backgroundColor = Color(0xFFE8E8E8)
                             ),
-                            shape = RoundedCornerShape(15.dp),
-                            onClick = {
-                                Log.d("TAG-Login", "firstName = $firstName, password = $password")
-                                val result = firstName.lowercase().replaceFirstChar{ it.uppercase() }
-                                vm.logIn(result)
+                            textStyle = textStyle,
+                            visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
+                            trailingIcon = {
+                                IconButton(
+                                    onClick = { showPassword = !showPassword },
+                                    content = {
+                                        Icon(
+                                            painter = painterResource(id = if (showPassword) R.drawable.eye_visible else R.drawable.eye_hidden),
+                                            contentDescription = if (showPassword) "Hide password" else "Show password"
+                                        )
+                                    }
+                                )
                             }
+                        )
+                    }
+
+                    Box(
+                        modifier = Modifier
+                            .padding(top = 50.dp, start = 44.dp, end = 44.dp)
+                            .fillMaxWidth()
+                    ) {
+                        Column(
                         ) {
-                            Text(
+                            Button(
                                 modifier = Modifier
-                                    .fillMaxSize()
-                                    .padding(vertical = 8.dp),
-                                text = "Login",
-                                fontFamily = FontFamily(Font(R.font.montserrat, FontWeight.Normal)),
-                                fontWeight = FontWeight.W700,
-                                fontSize = 14.sp,
-                                lineHeight = (17.07).sp,
-                                textAlign = TextAlign.Center,
-                                letterSpacing = (-0.3).sp,
-                                color = Color(0xFFEAEAEA)
-                            )
+                                    .fillMaxWidth()
+                                    .height(50.dp),
+                                colors = ButtonDefaults.buttonColors(
+                                    backgroundColor = Color(0xFF4E55D7),
+                                    contentColor = Color.White
+                                ),
+                                shape = RoundedCornerShape(15.dp),
+                                onClick = {
+                                    Log.d(
+                                        "TAG-Login",
+                                        "firstName = $firstName, password = $password"
+                                    )
+                                    val result =
+                                        firstName.lowercase().replaceFirstChar { it.uppercase() }
+                                    vm.logIn(result)
+                                    scope.launch {
+                                        if (!state.isLogged) {
+                                            scaffoldState.snackbarHostState.showSnackbar(
+                                                message = "User not exist",
+                                            )
+                                        }
+
+                                    }
+
+                                }
+                            ) {
+                                Text(
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .padding(vertical = 8.dp),
+                                    text = "Login",
+                                    fontFamily = FontFamily(
+                                        Font(
+                                            R.font.montserrat,
+                                            FontWeight.Normal
+                                        )
+                                    ),
+                                    fontWeight = FontWeight.W700,
+                                    fontSize = 14.sp,
+                                    lineHeight = (17.07).sp,
+                                    textAlign = TextAlign.Center,
+                                    letterSpacing = (-0.3).sp,
+                                    color = Color(0xFFEAEAEA)
+                                )
+                            }
                         }
                     }
-                }
 
+                }
             }
         }
-
     }
 }
